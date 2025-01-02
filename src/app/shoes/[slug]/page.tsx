aa6efeabdcec7552b41bbe340fb58bacc1b5fdd6
Michał Hawełka <m.hawelka@gmail.com>
2025-01-02 07:47:25 +0100

    Shoe page redesign part 1

--- src/app/shoes/[slug]/page.tsx
+++ src/app/shoes/[slug]/page.tsx
@@ -39,84 +39,6 @@ const ShoePage = async (props: { params: Params }) => {
             src={shoe.image.url}
           />
         )}
-      </div>
-      <div className="md:hidden">
-        {shoe.image && (
-          <img
-            className="w-full"
-            alt="image of running shoe"
-            src={shoe.image.url}
-          />
-        )}
-      </div>
-      <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
-        <div className="border-b border-gray-200 pb-6">
-          <p className="text-sm leading-none text-gray-600">
-            {shoe.shoeType.name} | {shoe.brand.name}
-          </p>
-          <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">
-            {shoe.name}
-          </h1>
-        </div>
-        <div className="py-4 border-b border-gray-200 flex items-center justify-between">
-          <p className="text-base leading-4 text-gray-800">Price</p>
-          <div className="flex items-center justify-center">
-            <p className="text-sm leading-none text-gray-600 ">
-              {shoe.pricePln}zł | {shoe.priceEur}€ | {shoe.priceUsd}$
-            </p>
-          </div>
-        </div>
-        {shoe.category && (
-          <div className="py-4 border-b border-gray-200 flex items-center justify-between">
-            <p className="text-base leading-4 text-gray-800 ">Category</p>
-            <div className="flex gap-2 items-center justify-center">
-              {shoe.category.map((cat, index) => (
-                <div
-                  className="center relative inline-block select-none whitespace-nowrap rounded-lg bg-blue-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white"
-                  key={`${shoe.name}-category-${index}`}
-                >
-                  <div className="mt-px">{cat}</div>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-
-        <div className="py-4 border-b border-gray-200">
-          <p className="text-base leading-4 text-gray-800">Specification</p>
-          <p className="text-base leading-4 mt-7 text-gray-600">
-            Men&apos;s weight: {shoe.weightM}g
-          </p>
-          <p className="text-base leading-4 mt-4 text-gray-600">
-            Men&apos;s drop: {shoe.dropM}mm
-          </p>
-          <p className="text-base leading-4 mt-4 text-gray-600">
-            Men&apos;s stack height: {shoe.heelStackM}mm -{" "}
-            {shoe.heelStackM - shoe.dropM}mm
-          </p>
-          <p className="text-base leading-4 mt-7 text-gray-600">
-            Women&apos;s weight: {shoe.weightW}g
-          </p>
-          <p className="text-base leading-4 mt-4 text-gray-600">
-            Women&apos;s drop: {shoe.dropW}mm
-          </p>
-          <p className="text-base leading-4 mt-4 text-gray-600">
-            Women&apos;s stack height: {shoe.heelStackW}mm -{" "}
-            {shoe.heelStackW - shoe.dropW}mm
-          </p>
-          <p className="text-base leading-4 mt-7 text-gray-600">
-            Foam: {shoe.foam}
-          </p>
-          <p className="text-base leading-4 text-gray-600 mt-4">
-            Plate: {shoe.plate}
-          </p>
-          <p className="text-base leading-4 text-gray-600 mt-4">
-            Outsole: {shoe.outsole}
-          </p>
-          <p className="text-base leading-4 text-gray-600 mt-4">
-            Notes: {shoe.notes}
-          </p>
-        </div>
         <div>
           <div className="border-t border-b py-4 mt-7 border-gray-200">
             <div
@@ -154,6 +76,292 @@ const ShoePage = async (props: { params: Params }) => {
           </div>
         </div>
         <button className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
+          <svg
+            className="mr-3 text-white"
+            width="16"
+            height="17"
+            viewBox="0 0 16 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.02301 7.18999C7.48929 6.72386 7.80685 6.12992 7.93555 5.48329C8.06425 4.83666 7.9983 4.16638 7.74604 3.55724C7.49377 2.94809 7.06653 2.42744 6.51835 2.06112C5.97016 1.6948 5.32566 1.49928 4.66634 1.49928C4.00703 1.49928 3.36252 1.6948 2.81434 2.06112C2.26615 2.42744 1.83891 2.94809 1.58665 3.55724C1.33439 4.16638 1.26843 4.83666 1.39713 5.48329C1.52583 6.12992 1.8434 6.72386 2.30968 7.18999L4.66634 9.54749L7.02301 7.18999Z"
+              stroke="currentColor"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M4.66699 4.83333V4.84166"
+              stroke="currentColor"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M13.69 13.8567C14.1563 13.3905 14.4738 12.7966 14.6025 12.15C14.7312 11.5033 14.6653 10.8331 14.413 10.2239C14.1608 9.61476 13.7335 9.09411 13.1853 8.72779C12.6372 8.36148 11.9926 8.16595 11.3333 8.16595C10.674 8.16595 10.0295 8.36148 9.48133 8.72779C8.93314 9.09411 8.5059 9.61476 8.25364 10.2239C8.00138 10.8331 7.93543 11.5033 8.06412 12.15C8.19282 12.7966 8.51039 13.3905 8.97667 13.8567L11.3333 16.2142L13.69 13.8567Z"
+              stroke="currentColor"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M11.333 11.5V11.5083"
+              stroke="currentColor"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          Check availability in store
+        </button>
+      </div>
+      <div className="md:hidden">
+        {shoe.image && (
+          <img
+            className="w-full"
+            alt="image of running shoe"
+            src={shoe.image.url}
+          />
+        )}
+      </div>
+      <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
+        <div className="border-b border-gray-200 pb-6">
+          <p className="text-sm leading-none text-gray-600">
+            {shoe.shoeType.name} | {shoe.brand.name}
+          </p>
+          <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">
+            {shoe.name}
+          </h1>
+        </div>
+
+        <div className="relative overflow-x-auto shadow-md my-4 sm:rounded-lg w-full 2xl:w-1/2">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="row" className="px-6 py-3">
+                  General info
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Price
+                </th>
+                <td className="px-6 py-4">
+                  {shoe.pricePln}zł | {shoe.priceEur}€ | {shoe.priceUsd}$
+                </td>
+              </tr>
+              {shoe.category && (
+                <tr className="odd:bg-white even:bg-gray-50 border-b">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    Category
+                  </th>
+                  <td className="px-6 py-4 flex gap-2">
+                    {shoe.category.map((cat, index) => (
+                      <div
+                        className="center relative inline-block select-none whitespace-nowrap rounded-lg bg-blue-500 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white"
+                        key={`${shoe.name}-category-${index}`}
+                      >
+                        <div className="mt-px">{cat}</div>
+                      </div>
+                    ))}
+                  </td>
+                </tr>
+              )}
+              {shoe.releaseDateEU && (
+                <tr className="odd:bg-white even:bg-gray-50 border-b">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    Release date
+                  </th>
+                  <td className="px-6 py-4 flex gap-2">
+                    {Intl.DateTimeFormat("en-GB", {
+                      month: "short",
+                      year: "numeric",
+                    }).format(new Date(shoe.releaseDateEU))}
+                  </td>
+                </tr>
+              )}
+              {shoe.releaseDateEU && (
+                <tr className="odd:bg-white even:bg-gray-50 border-b">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    Polish release date
+                  </th>
+                  <td className="px-6 py-4 flex gap-2">
+                    {Intl.DateTimeFormat("en-GB", {
+                      month: "short",
+                      year: "numeric",
+                    }).format(new Date(shoe.releaseDatePL))}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="row" className="px-6 py-3">
+                  Specification
+                </th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Men&apos;s weight
+                </th>
+                <td className="px-6 py-4">{shoe.weightM}g</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Men&apos;s drop
+                </th>
+                <td className="px-6 py-4">{shoe.dropM}mm</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Men&apos;s stack height
+                </th>
+                <td className="px-6 py-4">
+                  {shoe.heelStackM}mm - {shoe.heelStackM - shoe.dropM}mm
+                </td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Women&apos;s weight
+                </th>
+                <td className="px-6 py-4">{shoe.weightW}g</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Women&apos;s drop
+                </th>
+                <td className="px-6 py-4">{shoe.dropW}mm</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Women&apos;s stack height
+                </th>
+                <td className="px-6 py-4">
+                  {shoe.heelStackW}mm - {shoe.heelStackW - shoe.dropW}mm
+                </td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Stability
+                </th>
+                <td className="px-6 py-4">{shoe.stability}</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Foam
+                </th>
+                <td className="px-6 py-4">{shoe.foam}</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Plate
+                </th>
+                <td className="px-6 py-4">{shoe.plate}</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Outsole
+                </th>
+                <td className="px-6 py-4">{shoe.outsole}</td>
+              </tr>
+              <tr className="odd:bg-white even:bg-gray-50 border-b">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Notes
+                </th>
+                <td className="px-6 py-4">{shoe.notes}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="md:hidden">
+          <div className="border-t border-b py-4 mt-7 border-gray-200">
+            <div
+              data-menu
+              className="flex justify-between items-center cursor-pointer"
+            >
+              <p className="text-base leading-4 text-gray-800">Review</p>
+            </div>
+            <div
+              className="hidden pt-4 text-base leading-normal pr-12 mt-4 text-gray-600"
+              id="sect"
+            >
+              You will be responsible for paying for your own shipping costs for
+              returning your item. Shipping costs are nonrefundable
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden">
+          <div className="border-b py-4 border-gray-200">
+            <div
+              data-menu
+              className="flex justify-between items-center cursor-pointer"
+            >
+              <p className="text-base leading-4 text-gray-800">
+                Previous version
+              </p>
+            </div>
+            <div
+              className="pt-4 text-base leading-normal pr-12 mt-4 text-gray-600"
+              id="sect"
+            >
+              If you have any questions on how to return your item to us,
+              contact us.
+            </div>
+          </div>
+        </div>
+        <button className="md:hidden focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
           <svg
             className="mr-3 text-white"
             width="16"
--- src/app/shoes/page.tsx
+++ src/app/shoes/page.tsx
@@ -6,7 +6,7 @@ import ShoesListHeader from "./_components/ShoesListHeader";
 
 const SHOES_QUERY = `*[
   _type == "runningShoe" && defined(slug.current)
-]|order(lower(name) asc)[0...50]{_id, name, slug, releaseDatePL, image}`;
+]|order(lower(name) asc)[0...400]{_id, name, slug, releaseDateEU, image}`;
 
 export type SanityRunningShoe = SanityDocument & {
   name: string;
@@ -57,6 +57,34 @@ export default async function Shoes() {
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
       <h1 className="text-4xl font-bold mb-8">Running Shoes Index</h1>
+      <div className="flex gap-2">
+        <button
+          type="button"
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          All Shoes
+        </button>
+        <button
+          type="button"
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          2025
+        </button>
+        <button
+          type="button"
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          2024
+        </button>
+        <button
+          type="button"
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          This month
+        </button>
+      </div>
+
+      <h2 className="text-2xl font-bold mb-8">All items</h2>
       <ListView
         listViewHeader={<ShoesListHeader />}
         listViewItems={shoes.map((shoe) => (
