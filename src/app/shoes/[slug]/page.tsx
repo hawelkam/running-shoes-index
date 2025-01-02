f8077206180cb7c8bf515ff5ecd4be03e2fcfd79
Michał Hawełka <m.hawelka@gmail.com>
2025-01-02 06:53:35 +0100

    Compilation fix

--- src/app/shoes/[slug]/page.tsx
+++ src/app/shoes/[slug]/page.tsx
@@ -32,18 +32,22 @@ const ShoePage = async (props: { params: Params }) => {
   return (
     <div className="md:flex items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
       <div className="xl:w-2/6 lg:w-2/5 w-80 md:block hidden">
-        <img
-          className="w-full"
-          alt="image of running shoe"
-          src={shoe.image.url}
-        />
+        {shoe.image && (
+          <img
+            className="w-full"
+            alt="image of running shoe"
+            src={shoe.image.url}
+          />
+        )}
       </div>
       <div className="md:hidden">
-        <img
-          className="w-full"
-          alt="image of running shoe"
-          src={shoe.image.url}
-        />
+        {shoe.image && (
+          <img
+            className="w-full"
+            alt="image of running shoe"
+            src={shoe.image.url}
+          />
+        )}
       </div>
       <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
         <div className="border-b border-gray-200 pb-6">
