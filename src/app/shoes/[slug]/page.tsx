5d4d3b0c03a372cb9becfb84309365d584b3c1af
Michał Hawełka <m.hawelka@gmail.com>
2025-01-02 16:48:26 +0100

    Reviews section

--- src/app/shoes/[slug]/page.tsx
+++ src/app/shoes/[slug]/page.tsx
@@ -41,18 +41,49 @@ const ShoePage = async (props: { params: Params }) => {
         )}
         <div>
           <div className="border-t border-b py-4 mt-7 border-gray-200">
-            <div
-              data-menu
-              className="flex justify-between items-center cursor-pointer"
-            >
-              <p className="text-base leading-4 text-gray-800">Review</p>
-            </div>
-            <div
-              className="hidden pt-4 text-base leading-normal pr-12 mt-4 text-gray-600"
-              id="sect"
-            >
-              You will be responsible for paying for your own shipping costs for
-              returning your item. Shipping costs are nonrefundable
+            <div className="relative overflow-x-auto shadow-md my-4 sm:rounded-lg w-full 2xl:w-1/2">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="row" className="px-6 py-3">
+                      REVIEWS
+                    </th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="odd:bg-white even:bg-gray-50 border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      English reviews
+                    </th>
+                    <td className="px-6 py-4 flex gap-2">
+                      {shoe.ytReviewEN && <a href={shoe.ytReviewEN}>YouTube</a>}
+                      {shoe.igReviewEN && (
+                        <a href={shoe.igReviewEN}>Instagram</a>
+                      )}
+                      {shoe.ttReviewEN && <a href={shoe.ttReviewEN}>TikTok</a>}
+                    </td>
+                  </tr>
+                  <tr className="odd:bg-white even:bg-gray-50 border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      Polish reviews
+                    </th>
+                    <td className="px-6 py-4 flex gap-2">
+                      {shoe.ytReviewPL && <a href={shoe.ytReviewPL}>YouTube</a>}
+                      {shoe.igReviewPL && (
+                        <a href={shoe.igReviewPL}>Instagram</a>
+                      )}
+                      {shoe.ttReviewPL && <a href={shoe.ttReviewPL}>TikTok</a>}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
@@ -65,56 +96,49 @@ const ShoePage = async (props: { params: Params }) => {
               <p className="text-base leading-4 text-gray-800">
                 Previous version
               </p>
-            </div>
-            <div
-              className="pt-4 text-base leading-normal pr-12 mt-4 text-gray-600"
-              id="sect"
-            >
-              If you have any questions on how to return your item to us,
-              contact us.
+              <button className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
+                <svg
+                  className="mr-3 text-white"
+                  width="16"
+                  height="17"
+                  viewBox="0 0 16 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7.02301 7.18999C7.48929 6.72386 7.80685 6.12992 7.93555 5.48329C8.06425 4.83666 7.9983 4.16638 7.74604 3.55724C7.49377 2.94809 7.06653 2.42744 6.51835 2.06112C5.97016 1.6948 5.32566 1.49928 4.66634 1.49928C4.00703 1.49928 3.36252 1.6948 2.81434 2.06112C2.26615 2.42744 1.83891 2.94809 1.58665 3.55724C1.33439 4.16638 1.26843 4.83666 1.39713 5.48329C1.52583 6.12992 1.8434 6.72386 2.30968 7.18999L4.66634 9.54749L7.02301 7.18999Z"
+                    stroke="currentColor"
+                    strokeWidth="1.25"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M4.66699 4.83333V4.84166"
+                    stroke="currentColor"
+                    strokeWidth="1.25"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M13.69 13.8567C14.1563 13.3905 14.4738 12.7966 14.6025 12.15C14.7312 11.5033 14.6653 10.8331 14.413 10.2239C14.1608 9.61476 13.7335 9.09411 13.1853 8.72779C12.6372 8.36148 11.9926 8.16595 11.3333 8.16595C10.674 8.16595 10.0295 8.36148 9.48133 8.72779C8.93314 9.09411 8.5059 9.61476 8.25364 10.2239C8.00138 10.8331 7.93543 11.5033 8.06412 12.15C8.19282 12.7966 8.51039 13.3905 8.97667 13.8567L11.3333 16.2142L13.69 13.8567Z"
+                    stroke="currentColor"
+                    strokeWidth="1.25"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M11.333 11.5V11.5083"
+                    stroke="currentColor"
+                    strokeWidth="1.25"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                Compare with previous version
+              </button>
             </div>
           </div>
         </div>
-        <button className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
-          <svg
-            className="mr-3 text-white"
-            width="16"
-            height="17"
-            viewBox="0 0 16 17"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.02301 7.18999C7.48929 6.72386 7.80685 6.12992 7.93555 5.48329C8.06425 4.83666 7.9983 4.16638 7.74604 3.55724C7.49377 2.94809 7.06653 2.42744 6.51835 2.06112C5.97016 1.6948 5.32566 1.49928 4.66634 1.49928C4.00703 1.49928 3.36252 1.6948 2.81434 2.06112C2.26615 2.42744 1.83891 2.94809 1.58665 3.55724C1.33439 4.16638 1.26843 4.83666 1.39713 5.48329C1.52583 6.12992 1.8434 6.72386 2.30968 7.18999L4.66634 9.54749L7.02301 7.18999Z"
-              stroke="currentColor"
-              strokeWidth="1.25"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M4.66699 4.83333V4.84166"
-              stroke="currentColor"
-              strokeWidth="1.25"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M13.69 13.8567C14.1563 13.3905 14.4738 12.7966 14.6025 12.15C14.7312 11.5033 14.6653 10.8331 14.413 10.2239C14.1608 9.61476 13.7335 9.09411 13.1853 8.72779C12.6372 8.36148 11.9926 8.16595 11.3333 8.16595C10.674 8.16595 10.0295 8.36148 9.48133 8.72779C8.93314 9.09411 8.5059 9.61476 8.25364 10.2239C8.00138 10.8331 7.93543 11.5033 8.06412 12.15C8.19282 12.7966 8.51039 13.3905 8.97667 13.8567L11.3333 16.2142L13.69 13.8567Z"
-              stroke="currentColor"
-              strokeWidth="1.25"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M11.333 11.5V11.5083"
-              stroke="currentColor"
-              strokeWidth="1.25"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          Check availability in store
-        </button>
       </div>
       <div className="md:hidden">
         {shoe.image && (
