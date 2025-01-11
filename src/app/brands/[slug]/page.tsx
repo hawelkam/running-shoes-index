2f7dc9ea22e66bd3ce0861a46dd761e0cb3696d5
Michał Hawełka <m.hawelka@gmail.com>
2025-01-11 07:46:00 +0100

    Brand page adjustments

--- src/app/brands/[slug]/page.tsx
+++ src/app/brands/[slug]/page.tsx
@@ -3,8 +3,8 @@ import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 import { SanityBrand } from "../page";
 import ListView from "@/_components/ListView";
-import ShoesListItem from "@/app/shoes/_components/ShoesListItem";
 import { SanityRunningShoe } from "@/app/shoes/page";
+import ShoeCard from "@/app/shoes/_components/ShoeCard";
 
 type Params = Promise<{ slug: string }>;
 async function getBrand(slug: string): Promise<SanityBrand | null> {
@@ -27,7 +27,7 @@ async function getShoesByBrand(slug: string): Promise<SanityRunningShoe[]> {
   try {
     const shoes = await client.fetch<SanityRunningShoe[]>(
       `*[
-  _type == "runningShoe" && defined(slug.current) && brand->.slug.current == "${slug}"]|order(lower(name) asc)[0...400]{_id, name, slug, image}`,
+  _type == "runningShoe" && defined(slug.current) && brand->.slug.current == "${slug}"]|order(lower(name) asc)[0...400]{_id, name, slug, shoeType->, category[]->, releaseInfo, image, review}`,
       {},
       { next: { revalidate: 60 } }
     );
@@ -53,26 +53,70 @@ const BrandPage = async (props: { params: Params }) => {
     <Suspense>
       <main className="container mx-auto min-h-screen max-w-5xl p-8">
         <h1 className="text-4xl font-bold mb-8">{brand.name}</h1>
-        <img
-          className="h-auto max-w-lg rounded-lg"
-          src={brand.image.url}
-          alt={brand.name}
-        />
-        <div>
-          <h2>Brand info</h2>
-          <ul>
-            <li>Country of Origin: {brand.country}</li>
-            <li>Official website: {brand.officialWebsite}</li>
-            <li>Polish website: {brand.polishWebsite}</li>
-            <li>Media contact: {brand.mediaContact}</li>
-            <li>Media contact for Poland: {brand.mediaContactPl}</li>
-          </ul>
+        <div className="flex gap-4 items-center">
+          <div className="max-w-sm bg-white">
+            <img
+              className="h-auto rounded-lg"
+              src={brand.image.url}
+              alt={brand.name}
+            />
+          </div>
+
+          <div className="py-4 w-full">
+            <div className="relative overflow-x-auto shadow-md my-4 sm:rounded-lg w-full">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="row" className="px-6 py-3">
+                      BRAND INFO
+                    </th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="odd:bg-white even:bg-gray-50 border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      Country of Origin
+                    </th>
+                    <td className="px-6 py-4 flex gap-2">{brand.country}</td>
+                  </tr>
+                  <tr className="odd:bg-white even:bg-gray-50 border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      Official website
+                    </th>
+                    <td className="px-6 py-4 flex gap-2">
+                      {brand.officialWebsite}
+                    </td>
+                  </tr>
+                  <tr className="odd:bg-white even:bg-gray-50 border-b">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                    >
+                      Media contact
+                    </th>
+                    <td className="px-6 py-4 flex gap-2">
+                      {brand.mediaContact}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-        <ListView
-          listViewItems={shoes.map((shoe) => (
-            <ShoesListItem shoe={shoe} key={shoe._id} />
-          ))}
-        />
+        <div className="p-4">
+          <ListView
+            listViewItems={shoes.map((shoe) => (
+              <ShoeCard shoe={shoe} key={shoe._id} />
+            ))}
+          />
+        </div>
       </main>
     </Suspense>
   );
