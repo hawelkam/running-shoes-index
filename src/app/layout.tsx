d1377608da1e641862f83daf3fdc5a1f5d8b8c0c
Michał Hawełka <m.hawelka@gmail.com>
2025-05-21 09:13:00 +0200

    Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
-import Link from "next/link";
 import SearchInput from "@/_components/SearchInput";
+import Sidebar from "@/_components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,52 +37,26 @@ export default function RootLayout({
           <Layout>
             <Header style={{ display: "flex", alignItems: "center" }}>
               <div className="demo-logo" />
-              <Menu
-                theme="dark"
-                mode="horizontal"
-                defaultSelectedKeys={["2"]}
-                items={[
-                  {
-                    key: 1,
-                    label: <Link href={`/brands`}>Brands</Link>,
-                  },
-                  {
-                    key: 2,
-                    label: <Link href={`/shoes`}>Shoes</Link>,
-                  },
-                  {
-                    key: 3,
-                    label: (
-                      <Link
-                        href={`/shoes/released/${Intl.DateTimeFormat("en-GB", {
-                          year: "numeric",
-                        }).format(new Date())}/${Intl.DateTimeFormat("en-GB", {
-                          month: "2-digit",
-                        }).format(new Date())}`}
-                      >
-                        This month&apos;s releases
-                      </Link>
-                    ),
-                  },
-                ]}
-                style={{ flex: 1, minWidth: 0 }}
-              />
               <div className="flex items-center">
                 <SearchInput />
               </div>
             </Header>
-            <Content>
-              <div
-                style={{
-                  background: "#f9f9f9",
-                  minHeight: 280,
-                  padding: 24,
-                  borderRadius: "5px",
-                }}
-              >
-                {children}
-              </div>
-            </Content>
+            <Layout style={{ display: "flex", flexDirection: "row" }}>
+              <Sidebar />
+              <Content>
+                <div
+                  style={{
+                    background: "#f9f9f9",
+                    minHeight: 280,
+                    padding: 24,
+                    borderRadius: "5px",
+                  }}
+                >
+                  {children}
+                </div>
+              </Content>
+            </Layout>
+
             <Footer style={{ textAlign: "center" }}>
               Michał Hawełka ©{new Date().getFullYear()}
             </Footer>
